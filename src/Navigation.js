8bbad447ae614f79893a84dd3c0cc7c6a959cefa
Manuel Ortega <manuelo@riafinancial.com>
2022-02-16 14:51:27 -0500

    chore: add information other patterns

--- src/Navigation.js
+++ src/Navigation.js
@@ -6,7 +6,8 @@ import {
     useMatch,
     useResolvedPath
   } from "react-router-dom";
-  
+  import Gist from "react-gist";
+
   import logo from './assets/logo.svg';
   import right  from './assets/right.svg';
   import { Card } from "./compound";
@@ -36,7 +37,7 @@ import {
         <BrowserRouter>
             <div className="main-layout">
                 <nav>
-                    <img src={ logo } alt="React Logo" />
+                    <img src={ logo } alt="Logo"  style={{maxWidth:'80%', marginBottom: '30px'}}/>
                     <h1>PATTERNS</h1>
                     <ul>
                         <li>
@@ -49,20 +50,29 @@ import {
                         </li>
                         <li>
                             <img src={ right } alt="" />
-                            <CustomLink to="/compoundv2">Compound V2</CustomLink>
+                            <img src={ right } alt="" />
+                            <CustomLink to="/compoundv2">Refactor V2</CustomLink>
                         </li>
                         <li>
                             <img src={ right } alt="" />
-                            <CustomLink to="/compoundv3">Compound V3</CustomLink>
+                            <img src={ right } alt="" />
+                            <CustomLink to="/compoundv3">Refactor V3</CustomLink>
                         </li>
                         <li>
                             <img src={ right } alt="" />
-                            <CustomLink to="/compoundv4">Compound V4</CustomLink>
-                        </li>
-                        <li>
                             <img src={ right } alt="" />
-                            <CustomLink to="/control-props">Control Props</CustomLink>
+                            <CustomLink to="/compoundv4">Refactor V4</CustomLink>
                         </li>
+                        <li></li>
+                        <li>Other Patterns</li>
+                        <li><img src={ right } alt="" /><CustomLink to="/control-props" style={{color:'gray'}}>Control Props</CustomLink></li>
+                        <li><img src={ right } alt="" /><CustomLink to="/context" style={{color:'gray'}}>Context Module</CustomLink></li>
+                        <li><img src={ right } alt="" /><CustomLink to="/flexible-compound" style={{color:'gray'}}>Flexible Compound</CustomLink></li>
+                        <li><img src={ right } alt="" /><CustomLink to="/props-collections" style={{color:'gray'}}>Prop Collections</CustomLink></li>
+                        <li><img src={ right } alt="" /><CustomLink to="/state-reducer" style={{color:'gray'}}>State Reducer</CustomLink></li>
+                    
+                     
+
                     </ul>
                 </nav>
                 <main>
@@ -225,8 +235,60 @@ import {
                             <>
                                 <h1>Pattern Control Props</h1>
                                 <span>It controls when the properties and state of our component change. It is used in forms</span>
+            
+                                <span><a href="https://advanced-react-patterns.netlify.app/6" >Recommended reading</a></span>
+                                {/* <Gist id='b132e39e54bf077b86043356c4f13066' file='states.js' /> */}
+                                
                             </>
                         } />
+                        <Route path="/context" element={
+                            <>
+                                <h1>Context Module Functions</h1>
+                                <span><a href="https://advanced-react-patterns.netlify.app/1" >Recommended reading</a></span>
+                                <span>EXAMPLE</span>
+                                <span>1.- Create Context</span>
+                                <span style={{width: '800px'}}><Gist id='29ddb298aff1efcf29c88f2418ba3e9e' file='context1.js' /></span>
+
+                                <span>2.- Create provider to Context</span>
+                                <span style={{width: '800px'}}><Gist id='0d613087f8814849467bd4921016ea54' file='context2.js' /></span>
+
+                                <span>3.- Create custom Hooks </span>
+                                <span style={{width: '800px'}}><Gist id='d6d8f6f1f304da8e8eb8658706f5d042' file='context3.js' /></span>
+
+
+                                <span>4.- Create Helper </span>
+                                <span style={{width: '800px'}}><Gist id='471a09b5f1ee24a2a474e159a828e2ac' file='helpers.js' /></span>
+
+            
+                                <span>5.- Hook Implementation </span>
+                                <span style={{width: '800px'}}><Gist id='893725e4ab2319cc6a86334264508ef3' file='App.js' /></span>
+                                
+                                <span><a href="https://vtechguys.medium.com/context-module-pattern-in-react-dd3e89d56f2d" >Recommended reading</a></span>
+
+                            </>
+                        } />
+                        <Route path="/flexible-compound" element={
+                            <>
+                                <h1>Flexible Compound components</h1>
+                                <span><a href="https://advanced-react-patterns.netlify.app/3" >Recommended reading</a></span>
+                            </>
+                        } />
+                        <Route path="/props-collections" element={
+                            <>
+                                <h1>Props collections and Getters</h1>
+                                <span><a href="https://advanced-react-patterns.netlify.app/4" >Recommended reading</a></span>
+                            </>
+                        } />
+
+                        <Route path="/state-reducer" element={
+                            <>
+                                <h1>State Reducer</h1>
+                                <span><a href="https://advanced-react-patterns.netlify.app/5" >Recommended reading</a></span>
+                                
+                            </>
+                        } />
+
+
                     </Routes>
                     
                     
